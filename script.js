135c18720d0ef05424827b5b237bd1a0b964e7ec
Vlada-WoodyChick <uchusa@mail.ru>
2023-11-06 13:51:58 +0300

    variables, functions, data type, comparison operators end

---
+++ script.js
@@ -0,0 +1,24 @@
+const message = 'Hello, world';
+document.write(message);
+document.write('<br>');
+const message_2 = message;
+document.write(message_2);
+document.write('<br>');
+let variable;
+document.write(variable);
+document.write('<br>');
+// document.write(variable_1); undefind
+document.write('<br>');
+let count = 10;
+document.write(count++); //10
+document.write('<br>');
+document.write(count);
+document.write('<br>');
+count = 10;
+document.write(++count); //11
+document.write('<br>');
+document.write(count); //11
+document.write('<br>');
+let x = 10;
+let y = 15;
+document.write(x > y); // false;
