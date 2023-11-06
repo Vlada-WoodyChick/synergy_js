18aa85aa688b2c4df82023ee34b87af51e53c50c
Vlada-WoodyChick <uchusa@mail.ru>
2023-11-06 16:58:13 +0300

    conditional statements, cycles

--- script.js
+++ script.js
@@ -1,24 +1,84 @@
-const message = 'Hello, world';
-document.write(message);
-document.write('<br>');
-const message_2 = message;
-document.write(message_2);
-document.write('<br>');
-let variable;
-document.write(variable);
-document.write('<br>');
-// document.write(variable_1); undefind
-document.write('<br>');
-let count = 10;
-document.write(count++); //10
-document.write('<br>');
-document.write(count);
-document.write('<br>');
-count = 10;
-document.write(++count); //11
-document.write('<br>');
-document.write(count); //11
-document.write('<br>');
-let x = 10;
-let y = 15;
-document.write(x > y); // false;
+let num = 5;
+if(num % 2) {
+    document.write('odd');
+    document.write('<br>');
+}else{
+    document.write('even');
+    document.write('<br>'); 
+}
+num = 6;
+if(num % 2) {
+    document.write('odd');
+    document.write('<br>');
+}else{
+    document.write('even');
+    document.write('<br>'); 
+}
+
+const evenOrOdd =(num) => {
+    num % 2 ?  document.write('number odd')  : document.write('number even');
+    document.write('<br>'); 
+}
+
+evenOrOdd(6);
+evenOrOdd(7);
+
+
+const countWhile = (num) => {
+    while(num > 0 && num < 10 ) {
+        document.write(num);
+        document.write('<br>'); 
+        num++;
+        
+}
+    }
+
+    countWhile(2);
+    document.write('<br>'); 
+    countWhile(0);
+    document.write('<br>'); 
+    document.write('<br> <br>'); 
+
+const countDoWhile =(num) => {
+    do{
+        document.write(num)
+        num++;
+        document.write('<br>'); 
+    }while(num > 0 && num < 10)
+    document.write('<br> <br>'); 
+}
+countDoWhile(20);
+document.write('<br>'); 
+countDoWhile(0);
+document.write('<br>'); 
+countDoWhile(-1);
+document.write('<br>'); 
+countDoWhile(5);
+document.write('<br> <br>'); 
+
+for(i = 0; i <= 4; i++) {
+    document.write(`i = ${i}`); 
+    document.write('<br>');  
+}
+document.write('<br> <br>'); 
+
+for(i = 0; i <= 8; i++) {
+    document.write(`i = ${i}`); 
+    document.write('<br>'); 
+    if(i === 5) break;
+}  
+document.write('<br> <br>');
+for(i = 0; i <= 8; i++) {
+    if(i % 2)  continue //четные
+    document.write(`i = ${i}`); 
+    document.write('<br>'); 
+}  
+
+document.write('<br> <br>');
+for(i = 0; i <= 8; i++) {
+    if(!(i % 2))  continue //нечетные
+    document.write(`i = ${i}`); 
+    document.write('<br>'); 
+} 
+
+
